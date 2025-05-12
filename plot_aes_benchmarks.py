3e5ba173ec3bdeae6a7867bd2ffdd32222843112
Hiroki <hiroki11hanada@gmail.com>
2025-05-12 17:54:57 +0900

    runs from m4

--- plot_aes_benchmarks.py
+++ plot_aes_benchmarks.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load CSV
-df = pd.read_csv("aes_benchmark_results.csv")
+""" df = pd.read_csv("./container_out/aes_benchmark_results.csv") """
+df = pd.read_csv("m4_lima_aes_benchmark_results.csv")
 
 # Split algorithm into key size and mode
 df[["cipher", "key_size", "mode"]] = df["algorithm"].str.extract(r"(AES)-(\d+)-([A-Z0-9]+)")
